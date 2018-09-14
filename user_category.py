4595c3c21ed9661938b1634861b4533001bb7666
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-14 15:42:00 +0300

    put  input function in the main

--- user_category.py
+++ user_category.py
@@ -7,27 +7,6 @@ current_time = datetime.datetime.now()
 #pick current year
 current_year = current_time.year
 
-#make sure only int values are processed
-def user_age():
-        try:
-            #prompt user to enter their year of birth
-            birth_year = int(input("Enter your year of birth (eg 2000):"))
-            user_category_value = current_year - birth_year
-
-        except ValueError:
-            return "Sorry, please enter year in numbers"
-           
-        if birth_year < 0:
-            return "Your Entered a negative value"
-            
-        elif birth_year > current_year:
-            return "You cannot be born in future "
-            
-        elif birth_year < 1800:
-            return "You must be dead by now"
-            
-        return user_category(user_category_value)
-
  #find the user category from the user_age values provided      
 def user_category(user_value):
 
@@ -42,5 +21,25 @@ def user_category(user_value):
                 
     return cat_message
 
-#display user category
-print(user_age())   
+#make sure only int values are processed
+def user_age(birth_year):
+        if type(birth_year) != int:
+            raise ValueError("Sorry, please enter year in numbers")
+           
+        if birth_year < 0:
+            return "Your Entered a negative value"
+            
+        elif birth_year > current_year:
+            return "You cannot be born in future "
+            
+        elif birth_year < 1800:
+            return "You must be dead by now"
+        user_category_value = current_year - birth_year
+            
+        return user_category(user_category_value)
+ 
+
+if __name__ == "__main__":
+    #prompt user to enter their year of birth
+    birth_year = int(input("Enter your year of birth (eg 2000):"))
+    print(user_age(birth_year)) 
